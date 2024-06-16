0b437ea0e6287fd35135584187a4fc31c41bf7a8
root <root@ip-172-31-43-91.ap-northeast-1.compute.internal>
2024-06-16 12:28:40 +0000

    add plugin meow-lightbox

---
+++ wp-content/plugins/meow-lightbox/meow-lightbox.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Meow Lightbox
+Plugin URI: https://meowapps.com/plugin/meow-lightbox
+Description: Beautiful Lightbox designed for photography, displaying EXIF data. Highly optimized for speed and elegance. You’ll love it!
+Version: 5.2.0
+Author: Jordy Meow
+Author URI: https://meowapps.com
+Text Domain: meow-lightbox
+Domain Path: /languages
+*/
+
+if ( !defined( 'MWL_VERSION' ) ) {
+  define( 'MWL_VERSION', '5.2.0' );
+  define( 'MWL_PREFIX', 'mwl' );
+  define( 'MWL_DOMAIN', ' meow-lightbox' );
+  define( 'MWL_ENTRY', __FILE__ );
+  define( 'MWL_PATH', dirname( __FILE__ ) );
+  define( 'MWL_URL', plugin_dir_url( __FILE__ ) );
+}
+
+require_once( 'classes/init.php');
+
+?>
